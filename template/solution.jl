3f659a9601ac0e249baa4ae414472384ff578918
JFLiang <j.liang@optimumprime.nl>
2021-12-09 15:05:38 +0100

    solution templates

---
+++ template/solution.jl
@@ -0,0 +1,15 @@
+include("../utils/io.jl")
+
+function parse_input(path::String)
+    raw_str = input_to_raw_str(path)
+    
+end
+
+
+function solution(path::String)
+    print(parse_input(path))
+
+end
+
+
+solution("./input.txt")
